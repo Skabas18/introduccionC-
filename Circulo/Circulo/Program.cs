4ba519448c85ab84fb53305bee178da6fb40925c
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-10 08:23:22 -0500

    Creación del proyecto de Circulo donde encontramos la funcionalidad de las constantes con la funcion Math.Pi

---
+++ Circulo/Circulo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenido, calculemos círculos!");
+//Pi * radio*radio
+
+var resultado = 0d; //Forma de indicarle a c# que es un valor de tipo double
+
+Console.WriteLine("Ingrese el valor del radio");
+var radio=Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * radio * radio;
+Console.WriteLine("El área de tu círculo es de: "+ resultado);
