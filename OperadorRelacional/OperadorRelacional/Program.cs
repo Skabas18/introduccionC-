3f670292dfb85b9d2c1392acc9319164ffc3fc25
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-10 10:28:16 -0500

    Implementación de los operadores Relacionales

---
+++ OperadorRelacional/OperadorRelacional/Program.cs
@@ -0,0 +1,20 @@
+﻿var (valor1, valor2, valor3) = (10, 1, 2);
+Console.WriteLine("Los valores son: "+ valor1 + " " + valor2 + " " + valor3);
+
+bool resultado1 = valor1 == valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 == valor2 es: " + resultado1);
+
+bool resultado2 = valor1 != valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 != valor2 es: " + resultado2);
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 > valor2 es: " + resultado3);
+
+bool resultado4 = valor1 > valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 > valor2 es: " + resultado4);
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 >= valor2 es: " + resultado5);
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("El resultado de la comparaciom valor1 <= valor2 es: " + resultado6);
