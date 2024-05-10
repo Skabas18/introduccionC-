c76728192e2e580db8d6726c35f1ba00571a87d7
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-10 10:55:15 -0500

    Implementacion de los strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Sebastian Cabal ID!");
+int altura = 180;
+int edad = 23;
+string nombre = "Sebastian Cabal";
+string informacion = "Nació en Cali, es ingeniero de sistemas";
+var hobby = "Gamer";
+
+string tarjetaDeInformacion = $"La información de {nombre} es la siguiente: \n Su edad es {edad} años \n Su altura es {altura} cms \n Información relevante {informacion} \n además, su hooby es ser: {hobby}";
+
+Console.WriteLine(tarjetaDeInformacion);
